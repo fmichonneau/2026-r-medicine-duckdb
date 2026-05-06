e6d1468c0dd24fc0a0f5fd49a20540e81bab4764
François Michonneau <francois.michonneau@gmail.com>
2026-05-06 15:51:23 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## R/Medicine 2026 -- Tutorial Working with Larger than Memory Data in R
+
+Code and Data for the tutorial "Working with Larger than Memory Data in R".
+
---
+++ code.R
@@ -0,0 +1,6 @@
+## Make sure you have the {duckdb} and {dplyr} packages installed.
+
+library(duckdb)
+library(dplyr)
+
+## Follow along the tutorial by typing the code in this file.
